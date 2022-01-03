c5abcdc2569d39af81c48eaf6483d297accd4fe4
iknowmagic <ilan@iknowmagic.net>
2022-01-02 19:03:11 -0800

    yarn updates, base features

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,9 +3,27 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, provide } from 'vue'
+
+import { useBreakpoints } from '@vueuse/core'
+
+// @ts-expect-error js breakpoints
+import bkpoints from '../../windi.breakpoints'
 
 export default defineComponent({
-  name: 'MainLayout'
+  name: 'MainLayout',
+  setup() {
+    const breakpoints = useBreakpoints(bkpoints)
+    const sm = breakpoints.smaller('md')
+    const md = breakpoints.between('md', 'lg')
+    const lg = breakpoints.greater('lg')
+    const mq = { sm, md, lg }
+    provide('mq', mq)
+  }
 })
 </script>
+
+<style>
+:root {
+}
+</style>
--- windi.config.ts
+++ windi.config.ts
@@ -1,12 +1,36 @@
 import colors from 'windicss/colors'
 import { defineConfig } from 'windicss/helpers'
 
+import breakpoints from './windi.breakpoints'
+
 export default defineConfig({
-  extend: {
-    theme: {
-      colors: {
-        gray: colors.gray,
-        dark: colors.dark
+  theme: {
+    screens: breakpoints,
+    fontSize: {
+      base: '16px'
+    },
+    fontFamily: {
+      sans: ['Kumbh Sans', 'sans-serif'],
+      serif: ['Roboto Slab', 'serif'],
+      mono: ['Space Mono', 'monospace']
+    },
+
+    colors: {
+      gray: colors.gray,
+      dark: colors.dark,
+      blue: colors.blue,
+      sky: colors.sky,
+      'blue-gray': colors.blueGray,
+      design: {
+        theme: 'var(--theme-color)',
+        '1': '#F87070',
+        '2': '#70F3F8',
+        '3': '#D881F8',
+        '4': '#D7E0FF',
+        '5': '#1E213F',
+        '6': '#FFFFFF',
+        '7': '#EFF1FA',
+        '8': '#161932'
       }
     }
   }
